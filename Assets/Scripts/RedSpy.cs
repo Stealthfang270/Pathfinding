ecc7431484d863895e498e5814346951bca3083b
Stealthfang270 <mattdragon03@gmail.com>
2023-11-22 12:14:11 -0700

    Added red spy states

--- Assets/Scripts/RedSpy.cs
+++ Assets/Scripts/RedSpy.cs
@@ -4,18 +4,72 @@ using UnityEngine;
 
 public class RedSpy : MonoBehaviour
 {
-    public GameObject keyNode, jailDoorNode, disguiseNode, intelligenceDoorNode, intelligenceNode;
+    public GameObject startNode, keyNode, jailDoorNode, disguiseNode, intelligenceDoorNode, intelligenceNode;
+    public float disguiseTime, jailDoorTime, intelligenceGrabTime;
+    private float disguiseTimeLeft, jailDoorTimeLeft, intelligenceGrabTimeLeft;
     public Pathfinder pathfinder;
+    State state;
+    bool keyFound = false;
+    bool disguised = false;
+    bool hasIntelligence = false;
+    BoxCollider detector;
+    public GameObject lockedIntelDoor, lockedJailDoor;
 
     private void Awake()
     {
-        
+        state = State.KeySearch;
+        pathfinder.startNode = startNode;
+        pathfinder.destinationNode = keyNode;
+
+        disguiseTimeLeft = disguiseTime;
+        jailDoorTimeLeft = jailDoorTime;
+        intelligenceGrabTimeLeft = intelligenceGrabTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case State.KeySearch:
+                if(pathfinder.atDestination)
+                {
+                    keyFound = true;
+                    state = State.Infiltrate;
+                    pathfinder.destinationNode = intelligenceDoorNode;
+                    pathfinder.CreatePath();
+                }
+                break;
+            case State.Infiltrate:
+                if(pathfinder.atDestination)
+                {
+                    lockedIntelDoor.GetComponent<Pathnode>().activeNode = true;
+                    state = State.GrabIntelligence;
+                    pathfinder.destinationNode = intelligenceNode;
+                    pathfinder.CreatePath();
+                }
+                break;
+            case State.GrabIntelligence:
+                if(pathfinder.atDestination && intelligenceGrabTimeLeft > 0)
+                {
+                    intelligenceGrabTimeLeft -= Time.deltaTime;
+                }
+                else if(pathfinder.atDestination) 
+                {
+                    state = State.Escape;
+                    hasIntelligence = true;
+                    pathfinder.destinationNode = startNode;
+                    pathfinder.CreatePath();
+                }
+                break;
+            case State.Escape:
+                //Yeah nothing really needs to go here lmao
+                break;
+            case State.Jailed:
+                break;
+            case State.Disguise:
+                break;
+        }
     }
 
     public enum State
@@ -25,7 +79,6 @@ public class RedSpy : MonoBehaviour
         GrabIntelligence,
         Escape,
         Jailed,
-        Disguise,
-        Disguised
+        Disguise
     }
 }
